7a75c64b4d88fc9b8ba6ddba0657231a3588322d
Colonel-Top <promsurinm@hotmail.com>
2017-08-26 18:02:12 +0700

    Upload Stock Commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Computer Engineering 15 Random Code Take Line
---
+++ src/enigma.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import java.util.Random;
+import java.util.ArrayList;
+/**
+ *
+ * @author proms
+ */
+public class enigma {
+    String nameen[] = {
+    
+    };
+    public static void generate()
+    {
+        ArrayList random  = new ArrayList();
+        Random rand = new Random();
+        int i = 28;
+        while(i >= 0)
+        {
+            
+        }
+    }
+}
